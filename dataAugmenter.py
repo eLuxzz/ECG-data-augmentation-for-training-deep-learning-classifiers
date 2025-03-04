ce6bc370e51e4b66480a351a113354f59c49e816
AmjadAlakrami <amjad-2002-ak@hotmail.com>
2025-03-04 16:45:54 +0100

    Added 3 augmentation methods

--- dataAugmenter.py
+++ dataAugmenter.py
@@ -1,10 +1,27 @@
+import numpy as np 
+
 class DataAugmenter():
     def __init__(self):
         pass
-
-    def Augment(self, signalData):
-        print("augment")
-        return signalData
-    def Augment1(self, signalData):
-        print("augment1")
-        return signalData
+    
+    def add_gaussian_noise(self, signalData, mean=0, std=0.05):
+        signalData = np.squeeze(signalData)
+        noise = np.random.normal(mean, std, signalData.shape)
+        augmented_data = signalData + noise
+        return augmented_data
+    
+    def add_baseline_wander(self, signalData, frequency=0.5, amplitude=0.1, sampling_rate=500):
+        signalData = np.squeeze(signalData)
+        t = np.arange(len(signalData)) / sampling_rate
+        baseline_noise = amplitude * np.sin(2 * np.pi * frequency * t)
+        baseline_noise = baseline_noise[:, None]  # Alternativ metod
+        augmented_data = signalData + baseline_noise
+        return augmented_data
+    
+    def add_powerline_noise(self, signalData, frequency=50, amplitude=0.01, sampling_rate=500):
+        signalData = np.squeeze(signalData)
+        t = np.arange(len(signalData)) / sampling_rate
+        powerline_noise = amplitude * np.sin(2 * np.pi * frequency * t)
+        powerline_noise = powerline_noise[:, None]
+        augmented_data = signalData + powerline_noise
+        return augmented_data
--- dataplotter_tester.py
+++ dataplotter_tester.py
@@ -1,6 +1,7 @@
 from dataloader import Dataloader
 import argparse
 from  dataPlotter import DataPlotter
+from dataAugmenter import DataAugmenter
 
 parser = argparse.ArgumentParser(description='Train neural network.')
 parser.add_argument('path_train_hdf5', type=str,
@@ -13,18 +14,16 @@ args = parser.parse_args()
 
 d = Dataloader(args.path_train_hdf5, args.path_train_csv,None,None )
 base_data, _ = d.getBaseData_Sliced(1)
-augmented_data = base_data + 1 
-
-print(len(base_data))
-
+augmented_data, _ = d.getAugmentedData_Sliced(["add_powerline_noise", "add_baseline_wander"],1)
 
 
 plotter = DataPlotter(
     base_data, 
     augmented_data,
     same_graf=True,
-    sample_range=[1,500],
-    leads=[1,2]
+    sample_range=[1,2000],
+    leads=[1,2,3]
+    
 
 )
 
