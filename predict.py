2e93816d6778e36fdd7c3b68cda1b6840be6f6d4
eLuxzz <64693154+eLuxzz@users.noreply.github.com>
2025-03-21 13:37:16 +0100

    Changed pathing to use model name

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import numpy as np
 import warnings
 import argparse
+import os
 warnings.filterwarnings("ignore")
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
@@ -9,14 +10,14 @@ from fileloader import Fileloader
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get performance on test set from hdf5')
-    parser.add_argument('path_to_hdf5', type=str,
+    parser.add_argument('path_to_hdf5', type=str, #signals_test.hdf5
                         help='path to hdf5 file containing tracings')
-    parser.add_argument('path_to_model',  # or model_date_order.hdf5
+    parser.add_argument('path_to_model', #model.keras 
                         help='file containing training model.')
     parser.add_argument('--dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
-    parser.add_argument('--output_file', default="./dnn_output.npy",  # or predictions_date_order.csv
-                        help='output csv file.')
+    parser.add_argument('--output_folder', default="dnn_predicts",
+                        help='Output folder.')
     parser.add_argument('-bs', type=int, default=32,
                         help='Batch size.')
 
@@ -33,11 +34,14 @@ if __name__ == '__main__':
     model = load_model(args.path_to_model, compile=False)
     model.compile(loss='binary_crossentropy', optimizer=Adam())
     y_score = model.predict(seq,  verbose=1)
-    # # thresholds = 0.8
-    # # thresholds = np.array([0.7, 0.8, 0.8, 0.8,0.8])  # Custom threshold per class
-    # # y_binary = (y_score >= thresholds).astype(int)
+    # thresholds = 0.8
+    # thresholds = np.array([0.7, 0.8, 0.8, 0.8,0.8])  # Custom threshold per class
+    # y_binary = (y_score >= thresholds).astype(int)
     # y_binary = (y_score >= 0.8).astype(int)
-    # Generate dataframe
-    np.save(args.output_file, y_score)
+    
+    fileName = args.path_to_model.split("/")[-1]
+    output_path = os.path.join(args.output_folder, f"{str.removesuffix(fileName, ".keras")}")
+
+    np.save(output_path, y_score)
 
     print("Output predictions saved")
