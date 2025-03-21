cab9e06bd70adbec72e7bf02cdc2faa21ebbc1e9
eLuxzz <64693154+eLuxzz@users.noreply.github.com>
2025-03-21 12:12:52 +0100

    Fixed dataplotter to work with new dataloader

--- dataplotter_tester.py
+++ dataplotter_tester.py
@@ -1,9 +1,7 @@
 from dataloader import Dataloader
 import argparse
 from  dataPlotter import DataPlotter
-from dataAugmenter import DataAugmenter
-import numpy as np
-import matplotlib.pyplot as plt
+
 parser = argparse.ArgumentParser(description='Train neural network.')
 parser.add_argument('path_train_hdf5', type=str,
                     help='path to hdf5 file containing training tracings')
@@ -15,22 +13,34 @@ args = parser.parse_args()
 
 d = Dataloader(args.path_train_hdf5, args.path_train_csv,None,None)
 
-base_dataset = d.getBaseData_Sliced(1)
-augmented_dataset = d.getAugmentedData_Sliced(["add_powerline_noise", "add_baseline_wander"],1)
+base_dataset = d.getBaseData(1)
+augmented_dataset = d.getAugmentedData(["add_baseline_wander"],1)
 
 (b_sample) = base_dataset.take(1)
 (a_sample) = augmented_dataset.take(1)
-print(augmented_dataset)
-print(b_sample)
 
-# for image, label in b_sample:
-#     print(image, label)
-# plotter = DataPlotter(
-#     b_sample,
-#     a_sample,
-#     same_graf=True,
-#     sample_range=[1,5000],
-# )
+for sample in b_sample:
+    b_signal = sample
+    print(f"Shape after da: {sample[0].shape}, {sample[1].shape}")    
+for sample in a_sample:
+    a_signal = sample
+    print(f"Shape after da: {sample[0].shape}, {sample[1].shape}")    
+f = True
+for s in b_signal[0]:
+    if  f:
+        f = False
+        b = s
+f = True
+for s in a_signal[0]:
+    if  f:
+        f = False
+        a = s
+plotter = DataPlotter(
+    b,
+    a,
+    same_graf=True,
+    sample_range=[1,5000],
+)
 
-# plotter.plot()
+plotter.plot()
 
