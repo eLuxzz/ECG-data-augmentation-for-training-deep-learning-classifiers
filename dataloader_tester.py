f15ac0f9d0ffabf00418ecbcc0a0c069f79d2414
eLuxzz <64693154+eLuxzz@users.noreply.github.com>
2025-03-24 21:49:43 +0100

    Fixed tester files to work with new dataloader and dataaugmenter

--- dataloader_tester.py
+++ dataloader_tester.py
@@ -12,19 +12,21 @@ def main():
                         help='path to hdf5 file containing validation tracings')
     parser.add_argument('path_valid_csv', type=str,
                         help='path to csv file containing validation annotations')
-    parser.add_argument('--val_split', type=float, default=0.02,
-                        help='number between 0 and 1 determining how much of'
-                             ' the data is to be used for validation. The remaining '
-                             'is used for validation. Default: 0.02')
     parser.add_argument('--training_dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
     parser.add_argument('--validation_dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
+    parser.add_argument('--final_model_name', type=str, default='final_model',
+                        help='name of the output file')
+    parser.add_argument("--DA", type=str, nargs="+", default=[],
+                        help="name of DA methods to apply, leave empty for base set")
     args = parser.parse_args()
     
-    d = Dataloader(args.path_train_hdf5, args.path_train_csv,args.path_valid_hdf5, args.path_valid_csv, args.training_dataset_name, args.validation_dataset_name)
+    dataloader = Dataloader(args.path_train_hdf5, args.path_train_csv,args.path_valid_hdf5, args.path_valid_csv, args.training_dataset_name, 
+                            args.validation_dataset_name)
 
-    # dataset = d.getAugmentedData(["Augment", "Augment1"])
-    # dataset = d.getValidationData(100)
+    dataset = dataloader.getTrainingData(["time_warp_crop"], sliceIdx=60)
+
+    print(dataset)
 if __name__ == "__main__":    
     main()
--- dataplotter_tester.py
+++ dataplotter_tester.py
@@ -11,38 +11,51 @@ parser.add_argument('path_train_csv', type=str,
 
 args = parser.parse_args()
 
-d = Dataloader(args.path_train_hdf5, args.path_train_csv,None,None)
+d = Dataloader(args.path_train_hdf5, args.path_train_csv,None,None, DA_P=1)
 
-base_dataset = d.getBaseData(1)
-# augmented_dataset = d.getAugmentedData(["add_baseline_wander"],1)
-# augmented_dataset = d.getAugmentedData(["add_gaussian_noise"],1)
-augmented_dataset = d.getAugmentedData(["add_powerline_noise"],1)
+base_dataset = d.getTrainingData_Plot(sliceIdx=1)
+
+# augmented_dataset = d.getTrainingData_Plot(["add_gaussian_noise"],1)
+# augmented_dataset = d.getTrainingData_Plot(["add_powerline_noise"],1)
+augmented_dataset = d.getTrainingData_Plot(["time_warp_crop"],1)
+# augmented_dataset = d.getTrainingData_Plot(["add_time_warp"],1)
 
 (b_sample) = base_dataset.take(1)
 (a_sample) = augmented_dataset.take(1)
+# print(b_sample)
 
+# print(a_sample)
 for sample in b_sample:
     b_signal = sample
-    print(f"Shape after da: {sample[0].shape}, {sample[1].shape}")    
+    print(f"Base: Shape: {sample[0].shape}, {sample[1].shape}")    
 for sample in a_sample:
     a_signal = sample
-    print(f"Shape after da: {sample[0].shape}, {sample[1].shape}")    
+    print(f"DA: Shape after da: {sample[0].shape}, {sample[1].shape}")    
 f = True
 for s in b_signal[0]:
-    if  f:
+    if f:
         f = False
         b = s
 f = True
-for s in a_signal[0]:
+for d in a_signal[0]:
     if  f:
         f = False
-        a = s
+        a = d
+# print(b)
+# print("-----------------")
+# print(a)
 plotter = DataPlotter(
     b,
     a,
     same_graf=True,
-    sample_range=[1,5000],
+    # sample_range=[900,1200],
+    sample_range=[0,5000],
 )
-
+plotter1 = DataPlotter(
+    b,
+    a,
+    same_graf=False,
+    sample_range=[0,5000],
+)
 plotter.plot()
-
+# plotter1.plot()
