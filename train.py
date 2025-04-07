6483fa3d3cbb3939abfd205d2de1036a559cb7f2
eLuxzz <64693154+eLuxzz@users.noreply.github.com>
2025-04-07 09:04:10 +0200

    Update

--- train.py
+++ train.py
@@ -6,14 +6,15 @@ mixed_precision.set_global_policy('mixed_float16')
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import (ModelCheckpoint, TensorBoard, ReduceLROnPlateau,
                                         EarlyStopping)
-from tensorflow import keras
+from tensorflow.keras.metrics import AUC
 from sklearn.utils.class_weight import compute_class_weight
+import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import argparse
-import numpy as np
 import time
 from CustomCallbacks import UpdateDA
+# from CustomMetrics import f1_score_metrics
 
 from model import get_model
 from dataloader import Dataloader
@@ -40,23 +41,31 @@ if __name__ == "__main__":
                         help='name of the output file')
     parser.add_argument("--DA", type=str, nargs="+", default=[],
                         help="name of DA methods to apply, leave empty for base set")
-
+    parser.add_argument("--BS", type=int, default=32,
+                        help="Custom Batchsize (Optional)")
+    parser.add_argument("--Slice", type=int, default=None,
+                        help="Slice dataset (Optional)")
+    parser.add_argument("--L2", type=float, default=0,
+                        help="L2 value (Optional)")
+    parser.add_argument("--ES", type=bool, default=True,
+                        help="Early Stopping, default True (Optional)")
     args = parser.parse_args()
 
     # Optimization settings
     lr = 0.002
-    batch_size = 32
+    batch_size = args.BS
     
     # Load data and setup sequences
-    slice = None
+    slice = args.Slice
     buffer_size= 10000
-    num_epochs = 70
+    num_epochs = 50
     dropout_keep_prob = 0.5
     DA_P = 0.85
     DA2_P = 0.25
-    l2_lambda = 0
+    l2_lambda = args.L2
     DA_methods = args.DA
-    tf.keras.callbacks.Callback()
+    useEarlyStopping = args.ES
+    
     dataloader = Dataloader(args.path_train_hdf5, args.path_train_csv,args.path_valid_hdf5, args.path_valid_csv, args.training_dataset_name, 
                             args.validation_dataset_name, batch_size, buffer_size=buffer_size, DA_P=DA_P)
     validation_dataset = dataloader.getValidationData(sliceIdx=slice)
@@ -64,37 +73,44 @@ if __name__ == "__main__":
         print("Running base set")
     else:
         print(f"Running with Augmentations: {DA_methods}")
+    print(f"Batchsize: {batch_size}")
+    print(f"L2: {l2_lambda}")
     tf_dataset = dataloader.getTrainingData(DA_methods,sliceIdx=slice)
-    dataloader.update_DAMethod_index()
-    
 
     loss = 'binary_crossentropy'
     opt = Adam(lr)
-
+    metrics = [AUC(multi_label=True, name ="AUC"),
+                AUC(multi_label=True, curve="PR", name="pr_auc")
+                # ,f1_score_metrics # Får inte den att funka än, tanken är att byta ut EarlyStopping monitor till F1
+                ]
+    
     # If you are continuing an interrupted section, uncomment line bellow:
     # PATH_TO_PREV_MODEL = "backup_model_last.keras"
     # model = tf.keras.models.load_model(PATH_TO_PREV_MODEL, compile=False)
     model = get_model(dataloader.n_classes, dropout_keep_prob=dropout_keep_prob, l2_lambda=l2_lambda)
-    model.compile(loss=loss, optimizer=opt,  metrics = ["accuracy"])
+    model.compile(loss=loss, optimizer=opt,  metrics = metrics)
 
     # Create log
     log_dir = os.path.relpath("logs")  # Välj en enkel sökväg
     time_stamp = time.strftime("%Y%m%d-%H%M%S")
     os.makedirs(log_dir, exist_ok=True)
-    fileName = f"{args.final_model_name}-BS{batch_size}-LR{lr}-DR_Keep_P{dropout_keep_prob}-DA_P{DA_P}-DA2_P{DA2_P}-L2_{l2_lambda}-{time_stamp}"
+    fileName = f"{args.final_model_name}-BS{batch_size}-LR{lr}-DRK_P{dropout_keep_prob}-DA_P{DA_P}-DA2_P{DA2_P}-L2_{l2_lambda}-ES_{useEarlyStopping}-{time_stamp}"
     log_dir = os.path.join(log_dir, f"{fileName}")
 
     # Callbacks
     callbacks = [ReduceLROnPlateau(monitor='val_loss',
                                    factor=0.1,
-                                   patience=3,
-                                   min_lr=lr / 10000),
-                 EarlyStopping(patience=8,  # Patience should be larger than the one in ReduceLROnPlateau
-                               min_delta=0.00001,
-                               restore_best_weights = True, 
-                               verbose=1),
+                                   patience=4,
+                                   min_lr=1e-6),
                 UpdateDA(dataloader)]
-    callbacks += [TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True, profile_batch=10)]
+    if useEarlyStopping:
+        callbacks += [
+                    EarlyStopping(monitor="val_loss", # Tanken är att byta ut denna mot F1_Score
+                                patience=8,  # Patience should be larger than the one in ReduceLROnPlateau
+                                min_delta=0.00001,
+                                restore_best_weights = True, 
+                                verbose=1),]
+    callbacks += [TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True)]
         # Save the BEST and LAST model
     callbacks += [ModelCheckpoint('./backup_model_last.keras'),
                   ModelCheckpoint('./backup_model_best.keras', save_best_only=True),]
@@ -103,15 +119,18 @@ if __name__ == "__main__":
     print("Start training")
     # steps_per_epoch = (slice) // batch_size # Use this when testing with slice.
     # steps_per_epoch = len(trainAnnotationData) // batch_size
-    steps_per_epoch = dataloader.datasetLength // batch_size
-    
+    steps_per_epoch = 2*dataloader.datasetLength // batch_size
+    # dir_newmodels = os.path.relpath("new_models")
+    # os.makedirs(dir_newmodels, exist_ok=True)
+    # dir_newmodels = os.path.join(dir_newmodels, f"{fileName}")
     def training():
-        # # Compute class weights
+        # Compute class weights
+        # _, trainAnnotationData = dataloader.fileloader.getData(dataloader.PathHDF5_training, dataloader.SetName_Training, dataloader.PathCSV_training)
         # class_weights = compute_class_weight(class_weight="balanced",
         #                                     classes=np.unique(trainAnnotationData.argmax(axis=1)),
         #                                     y=trainAnnotationData.argmax(axis=1))
 
-        # Convert to dictionary format
+        # #Convert to dictionary format
         # class_weights_dict = {i: class_weights[i] for i in range(len(class_weights))}
         # print(class_weights_dict)
         
